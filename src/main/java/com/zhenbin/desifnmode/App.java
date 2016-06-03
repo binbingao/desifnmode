3000c83cccae6e406db44cceecdd167cea1eedb0
gao zhenbin <1483514847@qq.com>
2016-06-03 17:47:37 +0800

    test
    test

---
+++ src/main/java/com/zhenbin/desifnmode/App.java
@@ -0,0 +1,13 @@
+package com.zhenbin.desifnmode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
